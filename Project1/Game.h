65271cf59d7ab417882ec979c7cd63a13dad3dc3
samueldonner <samueldonner@Samuels-MacBook-Pro.local>
2016-01-09 01:21:39 -0800

    Initial Commit

---
+++ Project1/Game.h
@@ -0,0 +1,13 @@
+class Game
+{
+public:
+    // Constructor/destructor
+    Game(int rows, int cols, int nRobots);
+    ~Game();
+    
+    // Mutators
+    void play();
+    
+private:
+    Arena* m_arena;
+};
---
+++ Project1/History.h
@@ -0,0 +1,13 @@
+//
+//  History.h
+//  Project1
+//
+//  Created by Samuel Donner on 1/7/16.
+//  Copyright © 2016 Samuel Donner. All rights reserved.
+//
+
+#ifndef History_h
+#define History_h
+
+
+#endif /* History_h */
